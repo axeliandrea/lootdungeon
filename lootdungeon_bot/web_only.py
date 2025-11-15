8041b9be1fb4820caac428f6eba10bd54279122b
loot! <axeliandrea666@gmail.com>
2025-11-16 03:00:34 +0700

    Add files via upload

---
+++ lootdungeon_bot/web_only.py
@@ -0,0 +1,10 @@
+"""
+Alternative Web-only deployment for Railway
+Simplified entry point yang hanya run web app (tanpa bot)
+"""
+import os
+from web_app import app
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
